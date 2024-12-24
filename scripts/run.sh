1e8b9a74ea68273b02c97edc1a74f7e1e8760f71
Mohsen Rad <irregular.celery@gmail.com>
2024-12-24 21:59:38 +0330

    Change 'r' rate

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 xrdb merge ~/.Xresources
-xset r rate 200 50 &
+xset r rate 300 50 &
 picom &
 
-while type chadwm > /dev/null; do chadwm >> ~/.chadwm2.log && continue || break; done
+while type chadwm > /dev/null; do
+    chadwm >> ~/.chadwm2.log && continue || break;
+done
