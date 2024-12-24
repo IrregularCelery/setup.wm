2db451dce7e2de4905729d7f05806185527ba19b
Mohsen Rad <irregular.celery@gmail.com>
2024-12-24 17:18:21 +0330

    Update `run.sh` file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-xrdb merge ~/.Xresources 
-xbacklight -set 10 &
-feh --bg-fill ~/Pictures/wall/gruv.png &
+xrdb merge ~/.Xresources
 xset r rate 200 50 &
 picom &
 
-dash ~/.config/chadwm/scripts/bar.sh &
-while type chadwm >/dev/null; do chadwm && continue || break; done
+while type chadwm > /dev/null; do chadwm >> ~/.chadwm2.log && continue || break; done
